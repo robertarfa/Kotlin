adbd7c8c2e6814429331bb899289022f55188f2e
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-06-14 09:07:13 -0300

    heritage

---
+++ BootcampKotlinUdacity/src/main/kotlin/classes/book/Book.kt
@@ -0,0 +1,22 @@
+package classes.book
+
+open class Book(val title: String, val author: String) {
+
+    private var currentPage: Int = 1
+    fun readPage(){
+        currentPage++
+    }
+}
+
+class eBook(
+    title: String,
+    author: String,
+    var format: String = "text"):
+    Book(title, author){
+
+    private var wordsRead = 0
+
+        override fun readPage(){
+            wordsRead += 250
+        }
+}
